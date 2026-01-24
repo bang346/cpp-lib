9832baf21c0f3d6c74d815c20537b100dd5fe80c
marco <marcocastronovo@icloud.com>
2026-01-24 20:07:51 +0100

    Remove build folder from repo

--- build/tests/test_control[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[DUMMY.basic]=]  C:/Users/marco/OneDrive/Dokumente/15.CPP_LIB/build/tests/Debug/test_control.exe [==[--gtest_filter=DUMMY.basic]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[DUMMY.basic]=]  PROPERTIES WORKING_DIRECTORY C:/Users/marco/OneDrive/Dokumente/15.CPP_LIB/build/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  test_control_TESTS DUMMY.basic)
