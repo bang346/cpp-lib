1bd19b458c496e1d46240de4b719440f55113e6b
Marco Castronovo <marcocastronovo@macbookpro.speedport.ip>
2026-05-16 13:19:06 +0200

    stm32 spi started

--- Hal/stm32/stm32_spi/include/stm32_spi.hpp
+++ Hal/stm32/stm32_spi/include/stm32_spi.hpp
@@ -1,4 +1,18 @@
 #ifndef STM32_SPI_HPP
 #define STM32_SPI_HPP
 
+#include "bus_if.hpp"
+
+class stm32_spi : public bus_if
+{
+private:
+    /* data */
+public:
+    virtual int transmit(const uint8_t *const data, const uint8_t len) const;
+    virtual int receive(uint8_t *const data, const uint8_t len) const;
+};
+
+
+
+
 #endif
