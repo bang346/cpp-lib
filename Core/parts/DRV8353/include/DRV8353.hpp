acc7e4caad88beda06884b7ca4aba8d1119f522a
Marco Castronovo <marcocastronovo@macbookpro.speedport.ip>
2026-06-01 20:07:06 +0200

    Template class and Test added

--- Core/parts/DRV8353/include/DRV8353.hpp
+++ Core/parts/DRV8353/include/DRV8353.hpp
@@ -11,27 +11,34 @@
 /// @tparam EnPin       Target dependet Enable-GPIO implementation
 /// @tparam FaultPin    Target dependet CS-GPIO implementation
 /// @tparam Delay       Target dependet blocking delay implementation
-template<typename CsPin, typename EnPin, typename FaultPin, typename Delay>
-class DRV8353 {
+template <typename CsPin, typename EnPin, typename FaultPin, typename Delay>
+class DRV8353
+{
 public:
     /// @brief Class Constructor
     /// @note           explicit prohibits unwanted initializations
     /// @param spi      Spi-object
     explicit DRV8353(bus_if &spi)
-    : spi_(spi)
-    {}
+        : spi_(spi)
+    {
+    }
 
-    /// @brief 
-    /// @param delay 
-    /// @return 
+    /// @brief
+    /// @param delay
+    /// @return
     int init()
     {
         CsPin::high();
         return 0;
     }
+
+    /// @brief
+    /// @param addr     4-Bit register address
+    /// @return         0xffff = error,
+    ///                 otherwise the register value
     uint16_t ReadReg(const uint8_t &addr) const
     {
-        if(addr > DRV8353_DRIVER_CONFIG_ADDR)
+        if (addr > DRV8353_DRIVER_CONFIG_ADDR)
         {
             return 0xffff;
         }
@@ -49,11 +56,29 @@ public:
         return rx & DRV8353_REG_DATA_MASK;
     }
 
+    /// @brief
+    /// @param addr     4-Bit register address
+    /// @param data     10-Bit data
+    /// @return         0 = success,
+    ///                 0xffff = error
+    uint16_t WriteReg(const uint8_t &addr, const uint16_t &data)
+    {
+        if (addr > DRV8353_DRIVER_CONFIG_ADDR)
+        {
+            return 0xffff;
+        }
+        uint16_t data_rx = 0;
+        uint16_t data_tx = static_cast<uint16_t>((0U << 15) | ((addr & 0x0F) << 10) | (data & 0x3ff));
+        return spi_.transmitreceive(reinterpret_cast<uint8_t *>(&data_tx), 
+                                    reinterpret_cast<uint8_t *>(&data_rx), 
+                                    2
+                                    );
+    }
 
     bool fault() const { return FaultPin::read(); }
 
 private:
-    bus_if& spi_;
+    bus_if &spi_;
 };
 
 #endif
--- tests/parts/test_parts.cpp
+++ tests/parts/test_parts.cpp
@@ -12,15 +12,19 @@ using namespace ::testing;
 // GPIO-Policies (plattformabhängig)
 template<int Port, int Pin>
 struct GpioOut {
-    static inline void high() { std::cout << BOLDBLUE << "Set Port: " << Port << " Pin: " << Pin << RESET << std::endl; }
-    static inline void low()  { std::cout << BOLDBLUE << "Reset Port: " << Port << " Pin: " << Pin << RESET << std::endl; }
+    static inline void high() { std::cout << BOLDBLACK << "Set Port: " << Port << " Pin: " << Pin << RESET << std::endl; }
+    static inline void low()  { std::cout << BOLDBLACK << "Reset Port: " << Port << " Pin: " << Pin << RESET << std::endl; }
 };
 
 struct DelayPolicy {
-    static inline void us(uint32_t t) { std::cout << BOLDBLUE << "sleep for" << t << " us" << std::endl;}
-    static inline void ms(uint32_t t) { std::cout << BOLDBLUE << "sleep for" << t << " ms" << std::endl; }
+    static inline void us(uint32_t t) { std::cout << BOLDBLACK << "sleep for" << t << " us" << RESET << std::endl;}
+    static inline void ms(uint32_t t) { std::cout << BOLDBLACK << "sleep for" << t << " ms" << RESET << std::endl;}
 };
 
+struct GpioIn
+{
+    static inline bool read(){ std::cout << BOLDBLACK << "Gpio reads true" << RESET << std::endl; return true;}
+};
 
 
 
@@ -43,11 +47,12 @@ TEST(SPI, TransmitCalled)
     drv.ReadReg(0x00);
 
 }
-TEST(SPI, Example)
+
+TEST(SPI, ReadReg)
 {
     using CS    = GpioOut<1, 4>;
     using EN    = GpioOut<1, 5>;
-    using FAULT = GpioOut<1, 6>;
+    using FAULT = GpioIn;
     using DLY   = DelayPolicy;
 
     NiceMock<mock_spi> mock;
@@ -57,7 +62,7 @@ TEST(SPI, Example)
     const uint8_t fake_rx[]    = {0x55, 0x66};
 
     EXPECT_CALL(mock, transmitreceive(_, _, 2))
-        .WillOnce(Invoke([&](uint8_t* data_tx, uint8_t* data_rx, const uint8_t len) {
+        .WillOnce(Invoke([&](uint8_t* data_tx, uint8_t* data_rx, const uint8_t len){
             EXPECT_EQ(len, 2);
 
             EXPECT_THAT(
@@ -70,7 +75,40 @@ TEST(SPI, Example)
             return 0;
         }));
 
+    drv.fault();
     drv.ReadReg(0x00);
+}
+
+TEST(SPI, WriteReg)
+{
+    using CS    = GpioOut<1, 4>;
+    using EN    = GpioOut<1, 5>;
+    using FAULT = GpioIn;
+    using DLY   = DelayPolicy;
+
+    NiceMock<mock_spi> mock;
+    DRV8353<CS, EN, FAULT, DLY> drv(mock);
+
+    const uint8_t expected_tx[] = {0x1, 0xff};
+    const uint8_t fake_rx[]    = {0x55, 0x66};
+
+    EXPECT_CALL(mock, transmitreceive(_, _, 2))
+        .WillOnce(Invoke([&](uint8_t* data_tx, uint8_t* data_rx, const uint8_t len){
+            EXPECT_EQ(len, 2);
+
+            EXPECT_THAT(
+                std::vector<uint8_t>(data_tx, data_tx + len),
+                ElementsAreArray(expected_tx)
+            );
+
+            std::copy(fake_rx, fake_rx + len, data_rx);
+
+            return 0;
+        }));
+
+
+        drv.WriteReg(DRV8353_DRIVER_CONFIG_ADDR, 0x3ff);
+
 }
 
 
